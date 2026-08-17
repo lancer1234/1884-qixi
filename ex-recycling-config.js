6b95b2e51e4c4e83825c85381f307dc1418a2246
Danny <danny950811@gmail.com>
2026-08-17 16:42:53 +0800

    Add EX recycling cloud config

---
+++ ex-recycling-config.js
@@ -0,0 +1,6 @@
+// EX RECYCLING BIN cloud submission config.
+// After deploying google-apps-script/Code.gs as a Google Apps Script Web App,
+// paste the /exec URL below and commit this file.
+window.EX_RECYCLING_CONFIG = {
+  endpoint: ""
+};
